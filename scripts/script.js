46a40a214c649e672cb71783b12bb1573d500929
Lucia Lahučká <lahucka.lucia@pantarhei.sk>
2024-11-15 20:03:44 +0100

    refactor files to directories, create page for not implemented functions

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,22 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error('Error loading footer:', error));
 });
+
+function redirectToPage(page) {
+    switch (page) {
+        case 'adopt':
+            window.location.href = 'not_implemented.html';
+            break;
+        case 'walk':
+            window.location.href = 'walk_out.html';
+            break;
+        case 'help':
+            window.location.href = 'not_implemented.html';
+            break;
+        case 'contact':
+            window.location.href = 'not_implemented.html';
+            break;
+        default:
+            window.location.href = 'not_implemented.html';
+    }
+}
