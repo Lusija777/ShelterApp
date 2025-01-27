fefa3c670674453f86c81082dd9b674a1f3684b1
Anikanna44 <lenhardtova6@uniba.sk>
2025-01-27 11:15:35 +0100

    checkbox a walkId validation

--- scripts/walk_out_user_info.js
+++ scripts/walk_out_user_info.js
@@ -67,18 +67,18 @@ document.getElementById('registerForm').addEventListener('submit', function (eve
         showError('email', 'E-mail musí byť platný. Skontrolujte, či obsahuje @ a platnú doménu.');
         hasError = true;
     }
-
-    // Kontrola zhodnosti venčiarskeho preukazu
-    if (idNumber) {
-        const user = users.find(u => u.idNumber === idNumber);
-        if (!user) {
-            showError('walkId', 'Venčiarsky preukaz sa nenašiel medzi registrovanými užívateľmi.');
-            hasError = true;
-        } else if (user.name !== name || user.surname !== surname || user.phone !== phone || user.email !== email) {
-            showError('walkId', 'Údaje sa nezhodujú s existujúcim preukazom.');
-            hasError = true;
-        }
-    }
+    //
+    // // Kontrola zhodnosti venčiarskeho preukazu
+    // if (idNumber) {
+    //     const user = users.find(u => u.idNumber === idNumber);
+    //     if (!user) {
+    //         showError('walkId', 'Venčiarsky preukaz sa nenašiel medzi registrovanými užívateľmi.');
+    //         hasError = true;
+    //     } else if (user.name !== name || user.surname !== surname || user.phone !== phone || user.email !== email) {
+    //         showError('walkId', 'Údaje sa nezhodujú s existujúcim preukazom.');
+    //         hasError = true;
+    //     }
+    // }
 
     if (checkbox.checked){
         if (!password) {
